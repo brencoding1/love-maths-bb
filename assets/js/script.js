7cc0c3274a3b6d3608f0273c2ee5052c60add54b
brencoding1 <brencoding1@gmail.com>
2022-10-11 23:23:34 +0000

    add code to create two random numbers for the game question also I had to copy and paste from sourse code all js because I made a mistake and this was the easiest and quickest option

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
@@ -14,9 +16,18 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
+
 });
 
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
 function runGame() {
+
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
 }
 
@@ -36,7 +47,7 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
 
 }
 
